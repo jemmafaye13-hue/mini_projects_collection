3138275dfa185ee56b8b6d3b154fda026b175175
jemmafaye13-hue <jemma.faye13@gmail.com>
2026-04-23 20:40:35 +0800

    Added file handling and error management

--- highest_gwa/main_program.py
+++ highest_gwa/main_program.py
@@ -2,4 +2,9 @@ class GWAAnalyzer:
     def __init__(self, input_filename):
         self.input_filename = input_filename
 
-        
+    def find_highest_gwa(self):
+        try:
+            with open(self.input_filename, mode='r') as gwa_records:
+                pass  # Logic coming next
+        except FileNotFoundError:
+            print("File not found.")
